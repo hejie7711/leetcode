710c0f43fdc46ec4925b830993dc1f0d4565c02d
xiangzhai <xiangzhai83@gmail.com>
2014-01-23 20:34:47 +0800

    use makefile instead of scons

--- src/palindrome/palin_str.cpp
+++ src/palindrome/palin_str.cpp
@@ -5,7 +5,8 @@
 std::string m_preProcess(std::string s) 
 {
     int n = s.length();
-    if (n == 0) return "^$";
+    if (n == 0) 
+        return "^$";
     std::string ret = "^";
     for (int i = 0; i < n; i++)
         ret += "#" + s.substr(i, 1);
@@ -17,36 +18,29 @@ std::string m_preProcess(std::string s)
 std::string Manacher(std::string s) 
 {
     std::string T = m_preProcess(s);
-    std::cout << "PreProcessed: " << T << std::endl;
+    //std::cout << "预处理：" << T << std::endl;
     int n = T.length();
     int* P = new int[n];
     int C = 0, R = 0;
-    for (int i = 1; i < n - 1; i++) 
-    {
-        int i_mirror = 2 * C - i; // equals to i' = C - (i - C)
-    
+    for (int i = 1; i < n - 1; i++) {
+        int i_mirror = 2 * C - i; // 等于i' = C - (i - C)
         P[i] = (R > i) ? std::min(R-i, P[i_mirror]) : 0;
-    
-        // Attempt to expand palindrome centered at i
+        // 扩展回文中心点至i
         while (T[i + 1 + P[i]] == T[i - 1 - P[i]])
             P[i]++;
  
-        // If palindrome centered at i expand past R,
-        // adjust center based on expanded palindrome.
-        if (i + P[i] > R) 
-        {
+        // 如果回文中心i超与咯R，就调整回文中心。
+        if (i + P[i] > R) {
             C = i;
             R = i + P[i];
         }
     }
  
-    // Find the maximum element in P.
+    // 找出P的最大元素
     int maxLen = 0;
     int centerIndex = 0;
-    for (int i = 1; i < n - 1; i++) 
-    {
-        if (P[i] > maxLen) 
-        {
+    for (int i = 1; i < n - 1; i++) {
+        if (P[i] > maxLen) {
             maxLen = P[i];
             centerIndex = i;
         }
